6ad01b6f3785ef83ae238c19a2b58e7a99c0805e
Kubantov Muhamedzhan <mukhamedzhan.kubantov@narxoz.kz>
2026-05-02 09:01:34 +0500

    feat: implement runCouncil with multiple iterators and mediator dispatch

--- src/com/narxoz/rpg/council/CouncilEngine.java
+++ src/com/narxoz/rpg/council/CouncilEngine.java
@@ -2,7 +2,10 @@ package com.narxoz.rpg.council;
 
 import com.narxoz.rpg.combatant.Hero;
 import com.narxoz.rpg.guild.GuildMediator;
+import com.narxoz.rpg.quest.Quest;
+import com.narxoz.rpg.quest.QuestIterator;
 import com.narxoz.rpg.quest.QuestLog;
+import com.narxoz.rpg.quest.QuestPriority;
 import java.util.List;
 
 /**
@@ -14,6 +17,26 @@ public class CouncilEngine {
         // TODO: walk questLog with at least 2 different iterators,
         //       dispatch coordinating messages through hall for each quest,
         //       and return counters (questsTraversed, messagesRouted, membersNotified).
-        return new CouncilRunResult(0, 0, 0);
+        int questsTraversed = 0;
+        int messagesRouted = 0;
+        int membersNotified = 0;
+
+        QuestIterator orderedIter = questLog.ordered();
+        while (orderedIter.hasNext()) {
+            Quest quest = orderedIter.next();
+            questsTraversed++;
+            hall.dispatch("scouting", null, quest.getTitle());
+            messagesRouted++;
+        }
+
+        QuestIterator highPriorityIter = questLog.priorityAtLeast(QuestPriority.HIGH);
+        while (highPriorityIter.hasNext()) {
+            Quest quest = highPriorityIter.next();
+            questsTraversed++;
+            hall.dispatch("urgent", null, quest.getTitle());
+            messagesRouted++;
+        }
+
+        return new CouncilRunResult(questsTraversed, messagesRouted, membersNotified);
     }
 }
